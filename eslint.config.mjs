4a325aff6521bf9270865f3134f9876f3aec545e
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-07 12:03:56 -0300

    Set up TypeScript project with build and linting tools.
    Initialized a TypeScript project by adding essential configuration files such as `tsconfig.json`, `.gitignore`, and linting setup with ESLint, Husky, and lint-staged. Configured `package.json` with scripts for development and build processes, and added a simple "Hello World" script as an entry point in `src/index.ts`. This setup ensures consistent code quality and project structure.

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        ignores: [
+            "node_modules/**",
+            "dist/**",
+            "build/**",
+            ".lintstagedrc.js",
+            ".huskyrc.js",
+
+        ]
+    }
+);
