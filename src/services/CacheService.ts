a2978f986f54ed775951fbf05dcd2d46cb615382
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-08 14:35:29 -0300

    Add yarn.lock file to track dependencies
    This commit introduces a new yarn.lock file to the repository, which will enable precise tracking of exact package versions and their dependencies. This improves build consistency across different environments and ensures that the same package versions are used in every installation.

--- src/services/CacheService.ts
+++ src/services/CacheService.ts
@@ -1,14 +1,8 @@
-import {CacheManager} from "../lib/cache";
-
 export class CacheService {
 
 
-
-
     public getAllCaches() {
-        CacheManager.getInstance().on('get', (e,params) => {
-            const x = params[0]
-        })
+
     }
 
 }
